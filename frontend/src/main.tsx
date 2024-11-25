c776bd8dff6b588166fd2973ed879e0c5aa2fd2a
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2024-11-25 12:09:32 -0300

    feat: add Ant Design ConfigProvider with Portuguese locale support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,5 @@
+import { ConfigProvider } from "antd";
+import pt_BR from "antd/lib/locale/pt_BR";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
@@ -9,13 +11,15 @@ import SolicitacaoViagem from "./SolicitacaoViagem.tsx";
 const root = document.getElementById("root");
 // TODO - colocar layout padrão do header
 ReactDOM.createRoot(root!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/solicitacao-viagem" element={<SolicitacaoViagem />} />
-      <Route path="/opcoes-viagem/:customer_id" element={<OpcoesViagem />} />
-      <Route path="/historico" element={<Historico />} />
-      <Route path="*" element={<h1>Not Found</h1>} />
-    </Routes>
-  </BrowserRouter>
+  <ConfigProvider locale={pt_BR}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/solicitacao-viagem" element={<SolicitacaoViagem />} />
+        <Route path="/opcoes-viagem/:customer_id" element={<OpcoesViagem />} />
+        <Route path="/historico" element={<Historico />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  </ConfigProvider>
 );
