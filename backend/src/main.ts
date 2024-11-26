d650dd6a79f382cb4f04d8f2adf58b9249e1cbf4
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2024-11-26 20:21:46 -0300

    feat: implement global validation and custom HTTP exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { configDotenv } from 'dotenv';
 import { AppModule } from './app.module';
+import { CustomHttpExceptionFilter } from './common/filters/custom-http-exception.filter';
 
 configDotenv({ path: '../.env' });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new CustomHttpExceptionFilter());
 
   // TODO - Melhorar a documentação da API
   const config = new DocumentBuilder()
