9c4a51159f575734697e0898912feba49d143298
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2024-11-26 21:43:36 -0300

    feat: enhance ride request DTOs with validation for origin and destination fields

--- backend/src/modules/ride/dto/ride-confirm-req.dto.ts
+++ backend/src/modules/ride/dto/ride-confirm-req.dto.ts
@@ -1,9 +1,24 @@
+import { IsNotEmpty, IsString, NotEquals } from 'class-validator';
+
 export class RideConfirmReqDto {
+  @IsString()
+  @IsNotEmpty()
   customer_id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @NotEquals('destination')
   origin: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @NotEquals('origin')
   destination: string;
+
   distance: number;
   duration: string;
+
+  @IsNotEmpty()
   driver: {
     id: number;
     name: string;
--- backend/src/modules/ride/dto/ride-estimate-req.dto.ts
+++ backend/src/modules/ride/dto/ride-estimate-req.dto.ts
@@ -1,5 +1,17 @@
+import { IsNotEmpty, IsString, NotEquals } from 'class-validator';
+
 export class RideEstimateReqDto {
+  @IsString()
+  @IsNotEmpty()
   customer_id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @NotEquals('destination')
   origin: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @NotEquals('origin')
   destination: string;
 }
